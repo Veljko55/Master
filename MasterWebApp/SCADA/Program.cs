33c1d2345713d6472a7c8ff35bae329ae97a69db
veljko.djokic <veljko.djokic@wemedoo.com>
2021-01-09 14:09:42 +0100

    Import scada simulator into project and implementation of starting simulator on scada start

---
+++ MasterWebApp/SCADA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SCADA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartSimulator();
+            Console.ReadKey();
+        }
+
+        static void StartSimulator() 
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            var scadaFolderPath = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            Process.Start($"{scadaFolderPath}/EasyModbus/EasyModbusServerSimulator.exe");
+        }
+    }
+}
