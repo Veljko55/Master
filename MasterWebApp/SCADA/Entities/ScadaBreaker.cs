a4a2cf214973033ff0ed36770a8fdbedd5ba0d9d
veljko.djokic <veljko.djokic@wemedoo.com>
2021-01-09 14:44:16 +0100

    Initial scada db instance

---
+++ MasterWebApp/SCADA/Entities/ScadaBreaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCADA.Entities
+{
+    public class ScadaBreaker
+    {
+        public int Id { get; set; }
+        public bool IsOpet { get; set; }
+        public string ModelCode { get; set; }
+    }
+}
---
+++ MasterWebApp/SCADA/ScadaDB/ScadaContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+
+namespace SCADA.ScadaDB
+{
+    public class ScadaContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Entities.ScadaBreaker> Breakers { get; set; }
+
+        public ScadaContext()
+        {
+
+            Database.SetCommandTimeout((int)TimeSpan.FromMinutes(5).TotalSeconds);
+
+        }
+        public ScadaContext(DbContextOptions<ScadaContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Scada;Integrated Security=True;MultipleActiveResultSets=true;");
+        }
+    }
+}
